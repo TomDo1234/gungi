b4f1f97fedae6d9a40898dec37a0960ac5738eff
TomDo1234 <tomtheawesome123@gmail.com>
2023-09-16 13:17:12 +1000

    Link underlined

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -25,15 +25,21 @@
 	</div>
 	<div class="flex flex-col gap-y-12 container">
 		{#each piece_data as piece}
-			<PieceInfo { piece } />
+			<PieceInfo {piece} />
 		{/each}
 	</div>
 	<div class="container flex-col gap-y-5 text-xl">
 		<h1>Rules of Gameplay</h1>
-		<span
-	  >Visit the link to the <a href="https://www.reddit.com/r/HunterXHunter/comments/6803yz/revised_rules_of_gungi_in_hunterxhunter_pdf/" target="_blank" >original reddit post</a> which details the ruleset for Gungi and was the
-			main inspiration for this website.</span
-		>
+		<span>
+			Visit the link to the 
+			<a
+				href="https://www.reddit.com/r/HunterXHunter/comments/6803yz/revised_rules_of_gungi_in_hunterxhunter_pdf/"
+				target="_blank"
+				class="text-blue-400 underline">
+				original reddit post
+			</a> 
+			which details the ruleset for Gungi and was the main inspiration for this website.
+		</span>
 		<ol class="list-decimal [&>li]:ml-6 flex flex-col gap-y-4">
 			<li>Each player is provided a total of 38 pieces as enumerated above.</li>
 			<li>
@@ -51,22 +57,28 @@
 				A turn can be used for one of four things:
 				<div class="flex flex-col gap-y-4 ml-4 mt-4">
 					a. Move - choose one piece to move (see section above on how pieces move); remember that
-					towers themselves do not move, they only increase range of mobility of the top piece <br/><br/>
-					b. Attack - if an enemy piece occupies a square within your range of movement, you can
-					attack; if there is only one piece, your piece must then move into and occupy that square
-					but if you’re attacking a tower, the result is that the tower loses the top and your piece
-					now controls it <br/><br/>
-                    c. Stack - if either a friendly piece or an enemy piece is within your range
-					of movement, you can stack on top of that piece to form a tower (the top piece of the
-					tower controls the tower regardless of pieces of a different team below it) <br/><br/>
-                    d. Place - if you have less than the maximum pieces on the board, you may take a piece from your remaining
-					pieces (captured pieces cannot be used) and place it anywhere on the board with following
-					exceptions:
-                    <ol class="list-decimal ml-10 flex-col flex gap-y-4" >
-                        <li>Multiple pawns cannot be placed in the same file</li>
-                        <li>Pawns cannot place the opposing Marshal into check mate (although check is allowed)</li>
-                        <li>Pieces cannot be placed within the first three ranks of the opposing team's side of the board</li>
-                    </ol>
+					towers themselves do not move, they only increase range of mobility of the top piece <br
+					/><br />
+					b. Attack - if an enemy piece occupies a square within your range of movement, you can attack;
+					if there is only one piece, your piece must then move into and occupy that square but if you’re
+					attacking a tower, the result is that the tower loses the top and your piece now controls it
+					<br /><br />
+					c. Stack - if either a friendly piece or an enemy piece is within your range of movement, you
+					can stack on top of that piece to form a tower (the top piece of the tower controls the tower
+					regardless of pieces of a different team below it) <br /><br />
+					d. Place - if you have less than the maximum pieces on the board, you may take a piece from
+					your remaining pieces (captured pieces cannot be used) and place it anywhere on the board with
+					following exceptions:
+					<ol class="list-decimal ml-10 flex-col flex gap-y-4">
+						<li>Multiple pawns cannot be placed in the same file</li>
+						<li>
+							Pawns cannot place the opposing Marshal into check mate (although check is allowed)
+						</li>
+						<li>
+							Pieces cannot be placed within the first three ranks of the opposing team's side of
+							the board
+						</li>
+					</ol>
 				</div>
 			</li>
 			<li>
