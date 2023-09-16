25de7d3dfe5868ad78965e58e0f4f8c503511136
TomDo1234 <tomtheawesome123@gmail.com>
2023-09-16 17:54:50 +1000

    Captain rule successful

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -30,7 +30,8 @@ export function handleStockpileDnDConsider(e: CustomEvent, data: Item[]): Item[]
     return items
 }
 
-function getMovesIn2DForm({ display_name, current_level }: Piece,square_data: BoardState[number][number]): [number,number][] {
+function getMovesIn2DForm({ display_name, current_level }: Piece,square_data: BoardState[number][number],saved_level_for_captain: number | null = null): [number,number][] {
+    current_level = saved_level_for_captain ?? current_level;
     if (display_name === 'Pawn') {
         switch(current_level) {
             case 1:
@@ -43,9 +44,10 @@ function getMovesIn2DForm({ display_name, current_level }: Piece,square_data: Bo
         return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0]]
     }
     else if (display_name === 'Captain') {
-        if (square_data.pieces.length > 0) {
+        if (square_data.pieces.length > 1) {
+            //length + 1 line right below is for cases where the bottom piece is another captain;
             square_data.pieces.splice(0,1);
-            return getMovesIn2DForm(square_data.pieces[0],square_data);
+            return getMovesIn2DForm(square_data.pieces[0],square_data,current_level);
         }
         return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0]]
     }
@@ -200,7 +202,7 @@ export function availableMoves(piece: Piece | undefined,board_state: BoardState)
     }
     const { position } = piece;
 
-    const moves_in_2d = getMovesIn2DForm(piece,board_state[Math.floor(position / 9)][position % 9]);
+    const moves_in_2d = getMovesIn2DForm(piece,structuredClone(board_state[Math.floor(position / 9)][position % 9]));
 
     return getLegalMoves(position,moves_in_2d)
 }
