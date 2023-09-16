5eec7a04aa621ec8122750cdf2946aea0fff4d6c
TomDo1234 <tomtheawesome123@gmail.com>
2023-09-16 21:03:18 +1000

    General done

--- src/lib/game.ts
+++ src/lib/game.ts
@@ -30,164 +30,178 @@ export function handleStockpileDnDConsider(e: CustomEvent, data: Item[]): Item[]
     return items
 }
 
-function getMovesIn2DForm({ display_name, current_level }: Piece,board_state: BoardState,square_data: BoardState[number][number],saved_level_for_captain: number | null = null): [number,number][] {
+function getMovesIn2DForm({ display_name, current_level, position }: Piece, board_state: BoardState, square_data: BoardState[number][number], saved_level_for_captain: number | null = null): [number, number][] {
     current_level = saved_level_for_captain ?? current_level;
     if (display_name === 'Pawn') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,0]]
+                return [[-1, 0]]
             default:
-                return [[-1,0],[-1,-1],[-1,1]]
+                return [[-1, 0], [-1, -1], [-1, 1]]
         }
     }
     else if (display_name === "Marshal (King)" || display_name === "Fortress") {
-        return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0]]
+        return [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 1], [1, -1], [1, 0]]
     }
     else if (display_name === 'Captain') {
         if (square_data.pieces.length > 1) {
             //length + 1 line right below is for cases where the bottom piece is another captain;
-            square_data.pieces.splice(0,1);
-            return getMovesIn2DForm(square_data.pieces[0],board_state,square_data,current_level);
+            square_data.pieces.splice(0, 1);
+            return getMovesIn2DForm(square_data.pieces[0], board_state, square_data, current_level);
         }
-        return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0]]
+        return [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 1], [1, -1], [1, 0]]
     }
     else if (display_name === 'Spy') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,0]]
+                return [[-1, 0]]
             case 2:
-                return [[1,1],[1,-1],[-1,-1],[-1,1]]
+                return [[1, 1], [1, -1], [-1, -1], [-1, 1]]
             default: {
-                const moves: [number,number][] = [];
-                for (let i = 0;i < 9; i++) {
-                    moves.push([-i,-i])
-                    moves.push([-i,i])
-                    moves.push([i,i])
-                    moves.push([i,-i])
-                    moves.push([0,-i])
-                    moves.push([0,i])
-                    moves.push([i,0])
-                    moves.push([-i,0])
+                const moves: [number, number][] = [];
+                for (let i = 0; i < 9; i++) {
+                    moves.push([-i, -i])
+                    moves.push([-i, i])
+                    moves.push([i, i])
+                    moves.push([i, -i])
+                    moves.push([0, -i])
+                    moves.push([0, i])
+                    moves.push([i, 0])
+                    moves.push([-i, 0])
                 }
                 return moves;
             }
         }
     }
     else if (display_name === 'Cannon') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,0],[1,0],[0,1],[0,-1]]
+                return [[-1, 0], [1, 0], [0, 1], [0, -1]]
             case 2:
-                return [[-1,0],[1,0],[0,1],[0,-1],[-2,0],[2,0],[0,2],[0,-2]]
+                return [[-1, 0], [1, 0], [0, 1], [0, -1], [-2, 0], [2, 0], [0, 2], [0, -2]]
             default: {
-                const moves: [number,number][] = [];
-                for (let i = 0;i < 9; i++) {
-                    moves.push([0,-i])
-                    moves.push([0,i])
-                    moves.push([i,0])
-                    moves.push([-i,0])
+                const moves: [number, number][] = [];
+                for (let i = 0; i < 9; i++) {
+                    moves.push([0, -i])
+                    moves.push([0, i])
+                    moves.push([i, 0])
+                    moves.push([-i, 0])
                 }
                 return moves;
             }
         }
     }
     else if (display_name === 'Samurai') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[1,1],[1,-1],[-1,-1],[-1,1]]
+                return [[1, 1], [1, -1], [-1, -1], [-1, 1]]
             case 2:
-                return [[2,2],[2,-2],[-2,-2],[-2,2]]
+                return [[2, 2], [2, -2], [-2, -2], [-2, 2]]
             default: {
-                const moves: [number,number][] = [];
-                for (let i = 0;i < 9; i++) {
-                    moves.push([-i,-i])
-                    moves.push([-i,i])
-                    moves.push([i,i])
-                    moves.push([i,-i])
+                const moves: [number, number][] = [];
+                for (let i = 0; i < 9; i++) {
+                    moves.push([-i, -i])
+                    moves.push([-i, i])
+                    moves.push([i, i])
+                    moves.push([i, -i])
                 }
                 return moves;
             }
         }
     }
     else if (display_name === 'Musketeer') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,0]]
+                return [[-1, 0]]
             case 2:
-                return [[-1,0],[-2,0]]
+                return [[-1, 0], [-2, 0]]
             default: {
-                const moves: [number,number][] = [];
-                for (let i = 0;i < 9; i++) {
-                    moves.push([-i,0])
+                const moves: [number, number][] = [];
+                for (let i = 0; i < 9; i++) {
+                    moves.push([-i, 0])
                 }
                 return moves;
             }
         }
     }
     else if (display_name === 'Knight') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[0,-1],[0,1],[-2,1],[-2,-1]]
+                return [[0, -1], [0, 1], [-2, 1], [-2, -1]]
             case 2:
-                return [[-1,-2],[-1,2],[-2,1],[-2,-1]]
+                return [[-1, -2], [-1, 2], [-2, 1], [-2, -1]]
             default: {
-                return [[-1,-2],[-1,2],[-2,1],[-2,-1],[1,2],[1,-2],[2,-1],[2,1]]
+                return [[-1, -2], [-1, 2], [-2, 1], [-2, -1], [1, 2], [1, -2], [2, -1], [2, 1]]
             }
         }
     }
     else if (display_name === 'Archer') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0]]
+                return [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 1], [1, -1], [1, 0]]
             case 2:
-                return [[-2,0],[-2,-2],[-2,2],[0,2],[0,-2],[2,2],[2,-2],[2,0],[-1,-2],[-1,2],[-2,1],[-2,-1],[1,2],[1,-2],[2,-1],[2,1]]
+                return [[-2, 0], [-2, -2], [-2, 2], [0, 2], [0, -2], [2, 2], [2, -2], [2, 0], [-1, -2], [-1, 2], [-2, 1], [-2, -1], [1, 2], [1, -2], [2, -1], [2, 1]]
             default: {
-                return [[-3,-3],[-3,-2],[-3,-1],[-3,0],[-3,1],[-3,2],[-3,3],[3,-3],
-                [3,-2],[3,-1],[3,0],[3,1],[3,2],[3,3],[-2,3],[-1,3],[0,3],[1,3],[2,3],[-2,-3],[-1,-3],[0,-3],[1,-3],[2,-3]]
+                return [[-3, -3], [-3, -2], [-3, -1], [-3, 0], [-3, 1], [-3, 2], [-3, 3], [3, -3],
+                [3, -2], [3, -1], [3, 0], [3, 1], [3, 2], [3, 3], [-2, 3], [-1, 3], [0, 3], [1, 3], [2, 3], [-2, -3], [-1, -3], [0, -3], [1, -3], [2, -3]]
             }
         }
     }
     else if (display_name === 'General') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,0]]
+                return [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 0]]
             case 2:
-                return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0]]
+                return [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 1], [1, -1], [1, 0]]
             default: {
-                return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0],[-2,0],[-2,1],[-2,-1]]
+                const default_moves: [number, number][] = [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 1], [1, -1], [1, 0]];
+                const conditional_moves: [number, number][] = [[-2, 0], [-2, 1], [-2, -1]];
+                for (const move of conditional_moves) {
+                    if (position === undefined) {
+                        break;
+                    }
+
+                    const ver_position = Math.floor(position / 9) + move[0]; //move[0] is always negative 2 btw
+                    const not_out_of_bounds = position !== undefined && ver_position >= 0 && ver_position <= 8
+                    if (not_out_of_bounds && board_state[ver_position + 1][(position % 9) + move[1]]?.pieces.length > 0) {
+                        continue;
+                    }
+                    default_moves.push(move) //out of bounds is ok btw because getLegalMoves purges that anyway
+                }
+                return default_moves
             }
         }
     }
     else if (display_name === 'Lieutenant General') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,0],[-1,-1],[-1,1],[1,1],[1,-1]]
+                return [[-1, 0], [-1, -1], [-1, 1], [1, 1], [1, -1]]
             case 2:
-                return [[-1,0],[-1,-1],[-1,1],[1,1],[1,-1],[1,0]]
+                return [[-1, 0], [-1, -1], [-1, 1], [1, 1], [1, -1], [1, 0]]
             default: {
-                return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,1],[1,-1],[1,0]]
+                return [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 1], [1, -1], [1, 0]]
             }
         }
     }
     else if (display_name === 'Major General') {
-        switch(current_level) {
+        switch (current_level) {
             case 1:
-                return [[-1,1],[-1,-1]]
+                return [[-1, 1], [-1, -1]]
             case 2:
-                return [[-1,1],[-1,-1],[-1,0],[1,1],[1,-1]]
+                return [[-1, 1], [-1, -1], [-1, 0], [1, 1], [1, -1]]
             default: {
-                return [[-1,0],[-1,-1],[-1,1],[0,1],[0,-1],[1,0]]
+                return [[-1, 0], [-1, -1], [-1, 1], [0, 1], [0, -1], [1, 0]]
             }
         }
     }
     return []
 }
 
-function getLegalMoves(position: number,moves: [number,number][]): number[] {
+function getLegalMoves(position: number, moves: [number, number][]): number[] {
     const row = Math.floor(position / 9)
     const col = position % 9;
     const result: number[] = [];
-    for (const [vertical_movement,horizontal_movement] of moves) {
+    for (const [vertical_movement, horizontal_movement] of moves) {
         if (row + vertical_movement > 8 || row + vertical_movement < 0 || col + horizontal_movement > 8 || col + horizontal_movement < 0) {
             continue;
         }
@@ -196,18 +210,18 @@ function getLegalMoves(position: number,moves: [number,number][]): number[] {
     return result
 }
 
-export function availableMoves(piece: Piece | undefined,board_state: BoardState) {
+export function availableMoves(piece: Piece | undefined, board_state: BoardState) {
     if (!piece?.position) {
         return []
     }
     const { position } = piece;
 
-    const moves_in_2d = getMovesIn2DForm(piece,board_state,structuredClone(board_state[Math.floor(position / 9)][position % 9]));
+    const moves_in_2d = getMovesIn2DForm(piece, board_state, structuredClone(board_state[Math.floor(position / 9)][position % 9]));
 
-    return getLegalMoves(position,moves_in_2d)
+    return getLegalMoves(position, moves_in_2d)
 }
 
-export function availableStockpileMoves(piece: Piece | null,board_state: BoardState,players_ready: boolean,opponent_color: "white" | "black") {
+export function availableStockpileMoves(piece: Piece | null, board_state: BoardState, players_ready: boolean, opponent_color: "white" | "black") {
     if (!piece) {
         return []
     }
@@ -216,8 +230,8 @@ export function availableStockpileMoves(piece: Piece | null,board_state: BoardSt
 
     const pawn_taken_columns: number[] = [];
     const owned_by_opponent: number[] = [];
-    for (const [i,row] of board_state.entries()) {
-        for (const [j,square] of row.entries()) { 
+    for (const [i, row] of board_state.entries()) {
+        for (const [j, square] of row.entries()) {
             if (square.pieces?.[0]?.color === opponent_color) {
                 owned_by_opponent.push(i * 9 + j)
                 continue;
@@ -227,7 +241,7 @@ export function availableStockpileMoves(piece: Piece | null,board_state: BoardSt
                     pawn_taken_columns.push(j)
                     break;
                 }
-            }  
+            }
         }
     }
 
